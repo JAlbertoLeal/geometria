e41d957d86a358a6ab4ff9e5c8138f78efb0d718
Juan Alberto <juan.selfaleal@gmail.com>
2021-02-08 13:56:53 +0100

    Siempre positivo.

--- geometria/src/Circulo_JASL.java
+++ geometria/src/Circulo_JASL.java
@@ -6,6 +6,9 @@ public class Circulo_JASL extends FiguraGeometrica_JASL {
 	
 	public Circulo_JASL(double r, String tipoFigura) {
 		super(tipoFigura);
+		if(r<0) {
+			r= r*(-1);
+		}
 		radio = r;
 	}
 
