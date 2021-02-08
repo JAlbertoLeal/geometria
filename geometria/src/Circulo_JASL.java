17975a112e9bca0896c61be9e5dae97ceef31e49
Juan Alberto <juan.selfaleal@gmail.com>
2021-02-08 13:24:10 +0100

    Versión refactorizada

--- geometria/src/Circulo_JASL.java
+++ geometria/src/Circulo_JASL.java
@@ -1,10 +1,10 @@
 //Entornos de desarrollo. Práctica Geometría.
-public class Circulo extends FiguraGeometrica {
+public class Circulo_JASL extends FiguraGeometrica_JASL {
 	private double radio;
 	//Constante pi
 	static final double PI = 3.1416;
 	
-	public Circulo(double r, String tipoFigura) {
+	public Circulo_JASL(double r, String tipoFigura) {
 		super(tipoFigura);
 		radio = r;
 	}
